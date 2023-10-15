d6964d37a5dfc1e369f0d41f43ed2e80aa6ae4c9
Shahryar Tayeb <shahryar.dev@gmail.com>
2023-10-15 15:20:39 +0430

    WIP

--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,27 +1,27 @@
+class Node<T> {
+    value: T;
+    next: null;
+    constructor(value: T) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
 export default class SinglyLinkedList<T> {
-    public length: number;
+    // Indicates the number of items currently present in our list
+    public size: number;
+    public head:Node<T> | null;
 
-    
 
-    constructor() {
+    constructor(value: string | number, next: number) {
+        this.head = null;
+        this.size = 0;
     }
 
-    prepend(item: T): void {
-
-}
-    insertAt(item: T, idx: number): void {
-
-}
-    append(item: T): void {
-
-}
-    remove(item: T): T | undefined {
-
-}
-    get(idx: number): T | undefined {
-
-}
-    removeAt(idx: number): T | undefined {
-
-}
+    prepend(item: T): void {}
+    insertAt(item: T, idx: number): void {}
+    append(item: T): void {}
+    remove(item: T): T | undefined {}
+    get(idx: number): T | undefined {}
+    removeAt(idx: number): T | undefined {}
 }
