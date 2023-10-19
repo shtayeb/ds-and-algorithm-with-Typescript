1027a2c4914a9bceeb9f6d4ccedfc630944297c7
Shahryar Tayeb <shahryar.dev@gmail.com>
2023-10-19 15:16:28 +0430

    Tree traversing and search

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,22 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    // Base Case
+    if (!curr) {
+        return path;
+    }
 
+    // Recursion Steps
+    // 1 - pre
+
+    // 2 - recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+    
+    path.push(curr.value);
+    // 3 - post
+    return path;
+}
+
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,21 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(curr:BinaryNode<number> | null,path:number[]):number[]{
+    // Base Case
+    if(!curr){
+        return path;
+    }
 
+    // Recursion Steps
+    // 1 - pre
+    path.push(curr.value)
+
+    // 2 - recurse
+    walk(curr.left,path)
+    walk(curr.right,path)
+
+    // 3 - post
+    return path
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head,[])
 }
