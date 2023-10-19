ec0b566db6197c7a79d6efb1b5ad55138327012d
Shahryar Tayeb <shahryar.dev@gmail.com>
2023-10-19 16:13:22 +0430

    compare bs tree

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,22 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    // Base Case
+    // Structural check
+    if (a === null && b === null) {
+        return true;
+    }
 
+    // Structural check
+    if (a === null || b === null) {
+        return false;
+    }
+
+    // value check
+    if (a.value !== b.value) {
+        return false;
+    }
+
+    return compare(a.left, b.left) && compare(a.right, b.right);
 }
